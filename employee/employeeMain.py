03678ae3ef459d109ffeeff18d3a20033740bd07
Matthew <matthew2898@hotmail.com>
2022-05-31 11:03:00 -0400

    added employee test

---
+++ employee/employeeMain.py
@@ -0,0 +1,20 @@
+class employee:
+
+    def __init__(self, name, id, start, status):
+        self.name = name
+        self.id = id
+        self.start = start
+        self.status = status
+        print(name)
+
+    def getName():
+        return name
+    def getId():
+        return id
+    def getStart():
+        return start
+    def getStatus():
+        return status
+
+
+Matthew = employee('Matthew', 2341, '5/31/2022', 'Employed')
