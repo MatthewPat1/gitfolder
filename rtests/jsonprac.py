06c538ba43676f9749102b46a82e46f3eb6ee713
Matthew <matthew2898@hotmail.com>
2022-06-06 10:59:15 -0400

    json practice loading from file

--- rtests/jsonprac.py
+++ rtests/jsonprac.py
@@ -4,8 +4,20 @@ def jsonRun():
     jsonString = '{ "key1" : "value1", "key2" : "value2", "key3" : "value3" }'
     jsDict = json.loads(jsonString)
     print(jsDict['key2'])
-    with open('C:/Users/Matthew/Desktop/gitfolderT/gitpractice/fileMain/testfiles/text.txt', 'w+') as f:
+    with open('C:/Users/Matthew/Desktop/gitfolderT/gitpractice/fileMain/testfiles/prac.json', 'w+') as f:
         jsDump = json.dumps(jsDict)
         json.dump(jsDict, f)
         print(jsDump)
+
+
+
+def jsonLoad():
+
+    jsonString = '{ "key1" : "value1", "key2" : "value2", "key3" : "value3" }'
+
+    with open('C:/Users/Matthew/Desktop/gitfolderT/gitpractice/fileMain/testfiles/prac.json', 'r+') as f:
+        json_load = json.load(f)
+        print(json_load)
+
 jsonRun()
+jsonLoad()
